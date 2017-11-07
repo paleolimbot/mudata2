5de4c6709f49f1922b3e41b16d9a51e524273af0
Dewey Dunnington <dewey@fishandwhistle.net>
2017-11-07 18:10:46 -0500

    add ns_climate dataset

--- R/data.R
+++ R/data.R
@@ -64,3 +64,27 @@
 #' autoplot(kentvillegreenwood)
 #' 
 "kentvillegreenwood"
+
+#' Nova Scotia Long-Term Climate Data
+#'
+#' Monthly climate data for locations in Nova Scotia with records longer than 80 years.
+#'
+#' @format A \link{mudata} object
+#' 
+#' @source Environment Canada via the 'rclimateca' package. \url{http://climate.weather.gc.ca/}
+#' 
+#' @examples
+#' print(ns_climate)
+#' autoplot(ns_climate) # quite a messy plot, lots of data
+#' 
+#' # a more focused plot comparing three locations
+#' library(lubridate)
+#' ns_climate %>% 
+#'   select_locations(sable_island = starts_with("SABLE"),
+#'                    nappan = starts_with("NAPPAN"), 
+#'                    baddeck = starts_with("BADDECK")) %>% 
+#'   select_params(ends_with("temp")) %>%
+#'   filter_data(month(date) == 6) %>% 
+#'   autoplot()
+#' 
+"ns_climate"
--- data-raw/climate_data.R
+++ data-raw/climate_data.R
@@ -1,57 +1,81 @@
 
 library(tidyverse)
 library(mudata2)
+library(rclimateca)
+library(stringr)
 
-ecdata <- function(locs, ...) {
-  rclimateca::getClimateData(locs, ..., nicenames = TRUE) %>%
-    left_join(rclimateca::ecclimatelocs %>% 
-                select(station_name = Name, stationid = `Station ID`),
-              by = "stationid") %>%
-    select(station_name, year, month, date = parseddate, contains("temp"), contains("precip")) %>%
-    set_names(., stringr::str_replace(names(.), "(temp|precip)$", "_\\1") %>%
-                stringr::str_replace("(max|min)", "_\\1")) %>%
-    mutate(station_name = stringr::str_to_title(station_name)) %>%
-    as_tibble()
-}
+locs_tidy <- ecclimatelocs %>%
+  set_names(., tolower(names(.)) %>% str_replace("\\s\\(.*?\\)$", "") %>% str_replace_all("\\s", "_")) %>%
+  .[unique(names(.))] %>%
+  as_tibble() 
 
-eclocs <- function(locs) {
-  # get locations
-  rclimateca::ecclimatelocs %>%
-    filter(`Station ID` %in% locs) %>%
-    select(station_id = `Station ID`, location = Name, province = Province,
-           elevation_m = `Elevation (m)`, latitude = `Latitude (Decimal Degrees)`,
-           longitude = `Longitude (Decimal Degrees)`) %>%
-    mutate(location = stringr::str_to_title(location)) %>%
-    as_tibble()
-}
+ns_locs <- locs_tidy %>%
+  mutate(total_years = last_year - first_year) %>%
+  select(name, province, total_years, station_id, latitude, longitude, first_year, last_year) %>%
+  arrange(desc(total_years)) %>%
+  filter(province == "NOVA SCOTIA", total_years > 80)
 
-# get data for longest running climate stations in each province, write to canada_climate.csv
-canada_climate <- ecdata(c(5051, 5345, 735L, 3912, 2925, 6206, 2205, 6454, 
-                         1586, 1650, 6693, 1786, 6527)) %>%
-  group_by(station_name, year) %>%
-  summarise(mean_temp = mean(mean_temp, na.rm = TRUE), 
-            extr_max_temp = max(extr_max_temp, na.rm = TRUE),
-            extr_min_temp = min(extr_min_temp, na.rm = TRUE),
-            total_precip = mean(total_precip, na.rm = TRUE) * 12)
+ns_data_monthly <- getClimateData(ns_locs$station_id, timeframe = "monthly")
+ns_data_monthly_flags <- tibble(station_id = ns_locs$station_id) %>%
+  mutate(raw = map(station_id, getClimateDataRaw, timeframe = "monthly", 
+                   flag.info = TRUE, .cache = "ec.cache", .quiet = TRUE)) %>%
+  mutate(flags = map(raw, "flags")) %>%
+  pull(flags) %>%
+  bind_rows() %>%
+  distinct()
 
-canada_climate_locations <- eclocs(c(5051, 5345, 735L, 3912, 2925, 6206, 2205, 6454, 
-                                     1586, 1650, 6693, 1786, 6527))
+ns_data_monthly_tidy <- ns_data_monthly %>%
+  set_names(., tolower(names(.)) %>% str_replace("\\s\\(.*?\\)$", "") %>% str_replace_all("[^a-z0-9]+", "_")) %>%
+  .[unique(names(.))] %>%
+  as_tibble()
 
-canada_climate_long <- canada_climate %>%
-  ungroup() %>%
-  rename(location = station_name) %>%
-  gather(-location, -year, key = "param", value = "value") %>%
-  filter(is.finite(value))
+flags <- names(ns_data_monthly_tidy) %>% str_subset("flag$")
+vals <- str_replace(flags, "_flag$", "")
+nslong <- ns_data_monthly_tidy %>%
+  select(stationid, date = parseddate, one_of(vals), one_of(flags)) %>%
+  parallel_gather(value = one_of(vals), flag = one_of(flags), key = "param") %>%
+  mutate(flag = na_if(flag, ""), value = as.numeric(str_replace(value, "^<", ""))) %>%
+  filter(!(is.na(value) & is.na(flag))) %>%
+  rename(station_id = stationid)
 
-canada_climate_params <- canada_climate_long %>%
-  ungroup() %>%
-  distinct(param) %>%
-  mutate(unit = if_else(endsWith(param, "temp"), "degrees Celcius", "mm"))
-  
-canada_climate <- mudata(canada_climate_long, locations = canada_climate_locations,
-                         params = canada_climate_params, dataset_id = "ecclimate_yearly",
-                         x_columns = "year") %>%
-  update_datasets("ecclimate_yearly", url = "http://climate.weather.gc.ca/") %>%
-  validate_mudata()
+# ggplot(nslong, aes(date, value, col = factor(station_id))) +
+#   geom_line() +
+#   facet_wrap(~param, scales = "free_y")
 
+ns_climate_locs <- locs_tidy %>%
+  filter(station_id %in% unique(nslong$station_id)) %>%
+  mutate(location = paste(name, station_id))
+
+ns_climate_params <- tibble(
+  label = names(ns_data_monthly),
+  param = tolower(label) %>% 
+    str_replace("\\s\\(.*?\\)$", "") %>% 
+    str_replace_all("[^a-z0-9]+", "_")
+) %>%
+  filter(param %in% unique(nslong$param)) %>%
+  mutate(label = str_replace_all(label, "[^A-Za-z0-9'/() ]", "")) %>%
+  extract(label, "unit", "\\((.*?)\\)", remove = FALSE)
+
+ns_climate_data <- nslong %>%
+  left_join(ns_climate_locs %>% select(station_id, location), by = "station_id") %>%
+  left_join(ns_data_monthly_flags, by = "flag") %>%
+  select(-station_id) %>%
+  rename(flag_text = description)
+
+ns_climate <- mudata(
+  data = ns_climate_data,
+  locations = ns_climate_locs,
+  params = ns_climate_params,
+  dataset_id = "ecclimate_monthly",
+  x_columns = "date"
+) %>%
+  update_datasets("ecclimate_monthly", url = "http://climate.weather.gc.ca/") %>%
+  update_columns_table() %>%
+  update_columns("date", description = "First day of the month for which data are representative") %>%
+  update_columns(c("flag", "flag_text"), description = "Flags provide extra data to qualify value") %>%
+  update_columns("label", description = "Original Environment Canada column label") %>%
+  update_columns("unit", description = "Unit of measurement for values with this param")
+
+devtools::use_data(ns_climate, overwrite = TRUE)
+unlink("ec.cache", recursive = TRUE)
 

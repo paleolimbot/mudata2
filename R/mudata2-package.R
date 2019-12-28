fb96e096a8416b82ae23f6bf6d32c194baa3fbfb
Dewey Dunnington <dewey@fishandwhistle.net>
2019-12-28 15:27:40 -0400

    use_package_doc()

---
+++ R/mudata2-package.R
@@ -0,0 +1,51 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @importFrom rlang .data
+#' @importFrom rlang !!!
+#' @importFrom rlang !!
+#' @importFrom rlang enquo
+#' @importFrom rlang quos
+#' @importFrom rlang abort
+#' @importFrom dplyr n
+## usethis namespace: end
+NULL
+
+#' @export
+#' @importFrom dplyr everything
+dplyr::everything
+
+#' @export
+#' @importFrom dplyr matches
+dplyr::matches
+
+#' @export
+#' @importFrom dplyr starts_with
+dplyr::starts_with
+
+#' @export
+#' @importFrom dplyr ends_with
+dplyr::ends_with
+
+#' @export
+#' @importFrom dplyr contains
+dplyr::contains
+
+#' @export
+#' @importFrom dplyr num_range
+dplyr::num_range
+
+#' @export
+#' @importFrom dplyr one_of
+dplyr::one_of
+
+#' @importFrom dplyr src_tbls
+#' @export
+dplyr::src_tbls
+
+#' @importFrom magrittr %>%
+#' @export
+magrittr::`%>%`
